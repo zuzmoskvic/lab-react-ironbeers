4651dba8a859ebf4fd95071d62f4b74e258977c2
“Zuzana <zuzana.moskvic@gmail.com>
2025-07-30 11:09:53 +0200

    Add random beer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import AllBeers from './pages/AllBeers';
 import OneBeer from './pages/OneBeer';
+import RandomBeer from './pages/RandomBeer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element=<HomePage />>Home</Route>
-        <Route path='/beers' element=<AllBeers />>Home</Route>
-        <Route path='/beers/:id' element=<OneBeer />>Home</Route>
-        <Route path='/random' element=<HomePage />>Home</Route>
-        <Route path='/new' element=<HomePage />>Home</Route>
+        <Route path='/' element=<HomePage />/>
+        <Route path='/beers' element=<AllBeers />/>
+        <Route path='/beers/:id' element=<OneBeer />/>
+        <Route path='/random' element=<RandomBeer />/>
+        <Route path='/new' element=<HomePage />/>
         {/* <Route path='/search?q={query}' element=<HomePage />>Home</Route> */}
       </Routes>
     </div>
---
+++ src/pages/RandomBeer.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function RandomBeer() {
+    const [beer, setBeer] = useState({});
+
+    useEffect(()=>{
+        axios
+            .get("https://ih-beers-api2.herokuapp.com/beers/random")
+            .then((response)=>{
+                console.log(response.data)
+                setBeer(response.data);
+            })
+    }
+    ,[]);
+
+    return <div>
+                <img src={beer.image_url} alt="beer" width="300px"></img>
+                <h3>{beer.name}</h3>
+                <p>{beer.tagline}</p>
+                <p>{beer.first_brewed}</p>
+                <p>{beer.attenuation_level}</p>
+                <p>{beer.description}</p>
+                <p>{beer.contributed_by}</p>
+                <p></p>
+         </div>
+    
+}
+
+export default RandomBeer;
